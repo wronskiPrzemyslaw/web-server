d481e9474fa276eae45a4b1595d108f524374d1e
Przemyslaw Wronski <wronski.przemyslaw@gmail.com>
2016-09-18 17:00:50 +0200

    refactorization of middlewares

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 var express = require("express");
+var middleware = require("./middleware");
 var app = express();
 var PORT = 3000;
-
-var middleware = {
-    requireAuthentication: function(req, res, next) {
-        console.log("private route hit!");
-        next();
-    },
-    logger: function(req, res, next) {
-        console.log(new Date().toString() + " " + req.method + " " + req.originalUrl);
-        next();
-    }
-};
 
 app.use(middleware.logger);
 
